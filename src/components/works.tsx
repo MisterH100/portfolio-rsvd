71d6e13e9c68ae1fea9315f7933a4b46a4daa83d
misterh100 <handsomenyathi1@gmail.com>
2023-09-09 15:05:48 +0200

    fixed sizing of projects

--- src/components/works.tsx
+++ src/components/works.tsx
@@ -12,7 +12,7 @@ const Works = () => {
                     className="text-[2rem] font-bold whitespace-nowrap">My Work
                 </motion.h1>
             </div>
-            <div className="flex gap-[20px] flex-wrap justify-center px-[50px] md:px-[50px]">
+            <div className="w-full flex gap-[20px] flex-wrap justify-center md:px-[50px]">
                 {workData.map((data) => {
                     return (
                         <motion.article
@@ -23,14 +23,14 @@ const Works = () => {
                             className="light bg-primaryDark"
                             key={data.id}>
 
-                            <div className="bg-secondary w-[400px] min-w-[250px] h-[200px]">
+                            <div className="bg-secondary w-full md:w-[400px] min-w-[250px] h-[200px]">
                                 <img
                                     className="w-full h-full cursor-pointer"
                                     src ={data.img}
                                     alt={data.title}
                                 />
                             </div>
-                            <div className="w-[400px] min-w-[250px] p-[20px] md:p-[10px]">
+                            <div className="w-full md:w-[400px] min-w-[250px] p-[20px] md:p-[10px]">
                                 <p className="whitespace-wrap w-full underline text-white font-bold">{data.title}</p>
                                 <p className="whitespace-wrap h-[150px] w-full text-white">{ data.disc }</p>
                             </div>
