a02ef1727e5e5f6d3e2783ffcd1e0aab63d5bbc0
misterh100 <handsomenyathi1@gmail.com>
2023-09-19 00:28:29 +0200

    add backgrounds

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -1,8 +1,17 @@
 
 export const About =()=>{
     return(
-        <section id="about" className="w-full h-screen bg-about bg-center bg-cover px-10 md:px-20">
-            <h1>About</h1>
+        <section id="about" className="flex items-center w-full h-screen bg-about bg-center bg-cover px-10 md:px-20 pt-10">
+            <article className="w-full md:w-2/5  md:h-3/4 p-4 bg-gray-400 rounded-3xl backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-100">
+                <h1 className="w-full text-center h-20 text-4xl text-white font-medium  border-b border-gray-100">About</h1>
+                <p className="text-base md:text-lg tracking-wide pt-4">
+                    Greetings! I'm <span className="font-black">Handsome Nyathi</span>, a skilled web developer with a keen focus on UI/UX 
+                    design exploration.My work revolves around the dynamic <span className="underline">MERN</span> tech stack, enabling me 
+                    to craft immersive digital experiences.Backed by years of web development expertise,I'm committed to delivering polished 
+                    solutions.My boundlesse enthusiasm for technology drives my continuous learning and innovation.
+                    <span className="font-black">Let's collaborate and bring your digital visions to life!</span>
+                </p>
+            </article>
         </section>
     )
 }
--- src/components/sections/Home.tsx
+++ src/components/sections/Home.tsx
@@ -7,25 +7,28 @@ import { Button } from "../Button";
 
 export const Home =()=>{
     return(
-        <section id="home" className="relative w-full h-screen bg-home bg-center bg-cover px-10 md:px-20">
-            <article className="w-full md:w-2/4 flex justify-center md:justify-start pt-16 md:pt-40">
-                <div className="text-center md:text-left">
-                    <h1 className="text-primary text-sm sm:text-4xl uppercase">Handsome</h1>
-                    <h2 className="text-primary text-4xl sm:text-7xl font-extrabold uppercase">Nyathi</h2>
-                    <p className="text-primary text-sm sm:text-2xl font-thin sm:tracking-[0.5rem]">Web Developer</p>
+        <section id="home" className="relative file:flex flex-col w-full h-screen bg-home bg-center bg-cover px-10 md:px-20">
+            <article className="w-full flex flex-col items-start md:items-end">
+                <div className="w-full md:w-2/5 p-4 mt-10 md:mt-40 bg-gray-400 rounded-3xl backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-100">
+                    <h1 className="w-full text-center h-20 text-4xl text-white font-medium  border-b border-gray-100">Hello</h1>
+                    <div className="text-center pt-4">
+                        <h1 className="text-primary text-sm sm:text-4xl uppercase">Handsome</h1>
+                        <h2 className="text-primary text-4xl sm:text-7xl font-extrabold uppercase">Nyathi</h2>
+                        <p className="text-primary text-sm sm:text-2xl font-thin sm:tracking-[0.5rem]">Web Developer</p>
+                    </div>
+                </div>
+                <div className="w-full md:w-2/5 flex justify-center py-10">
+                    <Button text="Download" />
+                </div>
+                <div className="absolute w-full top-[80%] left-0 md:px-10 flex flex-col items-center md:items-start">
+                    <h3 className="text-primary hidden sm:block">follow:</h3>
+                    <ul className="flex items-center sm:py-2 gap-10">
+                        {socialsData.map(social=>
+                            <li key={social.id} className="active:scale-90"><Link to={social.link} target="_blank"><img src={social.logo}/></Link></li>
+                        )}
+                    </ul>
                 </div>
             </article>
-            <div className="w-full md:w-2/4 flex justify-center md:justify-start py-10">
-                <Button text="Download" />
-            </div>
-            <div className="w-full md:w-2/4 flex items-center flex-col md:items-start pt-1 md:pt-28 gap-2">
-                <h3 className="text-primary hidden sm:block">follow:</h3>
-                <ul className="flex items-center sm:py-2 gap-10">
-                    {socialsData.map(social=>
-                        <li key={social.id} className="active:scale-90"><Link to={social.link} target="_blank"><img src={social.logo}/></Link></li>
-                    )}
-                </ul>
-            </div>
         </section>
     )
 }
