ea4fad381bbd90ffe378f056030d368b9ad6d1ee
misterh100 <handsomenyathi1@gmail.com>
2023-09-23 11:16:39 +0200

    add goggle verification file

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Footer } from './components/Footer';
-import { Header } from './components/header';
+import { Header } from './components/Header';
 import { About } from './components/sections/About';
 import { Contact } from './components/sections/Contact';
 import { Home } from './components/sections/Home';
